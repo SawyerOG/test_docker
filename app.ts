e6a74aa443375d102d92b94819e4af845f0b4d52
SawyerOG <oglesbysaw@gmail.com>
2023-03-05 18:12:33 -0700

    nginx still

--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ import redis from './config/redis';
 redis.connect();
 
 const app = express();
-app.use(cors({ origin: ['127.0.0.1', '172.18.0.1'] }));
+app.use(cors({ origin: ['127.0.0.1', '172.*.*.*'] }));
 
 // log only 4xx and 5xx responses to console
 app.use(
